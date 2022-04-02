74c862d58f85ce5c46867ca6bdac96e50027c624
RadoStef <rs_perfect@mail.bg>
2022-04-03 00:41:10 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    $("button").click(function(){
+        $('.contact-section-up').slideToggle("slow");
+    });
+
+    $('img').zoomify();
+
+    $(".check").click(function(){
+        $('.available-on').slideToggle("slow");
+    });
+
+
+});
